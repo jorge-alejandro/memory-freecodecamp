6c2d7cae27ec8a6b7b30cab4db72cbdcdcdf1166
Yorch <gsgm1985@gmail.com>
2023-04-21 13:39:24 +0200

    Primer Commit con todo lo avanzado desde la mañana

---
+++ scripts.js
@@ -0,0 +1,36 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+    this.classList.add('flip');
+
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+
+        console.log({hasFlippedCard, firstCard});
+    } else {
+        hasFlippedCard = false;
+        secondCard = this;
+        
+        if ( firstCard.dataset.framework === secondCard.dataset.framework) {
+            firstCard.removeEventListener('click', flipCard);
+            secondCard.removeEventListener('click', flipCard);
+        } else {
+            setTimeout(() => {
+                firstCard.classList.remove('flip');
+                secondCard.classList.remove('flip');
+            }, 1500)
+            
+
+
+        };
+        console.log('Function works !!!');
+    };
+};
+
+
+cards.forEach(card => card.addEventListener('click',flipCard));
+
