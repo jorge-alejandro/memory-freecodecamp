b46434bcef4843c895a95270c1a60f9b0c72fd08
Yorch <20454656+jorge-alejandro@users.noreply.github.com>
2023-04-23 12:26:46 +0200

    finish the memory game standar logic

--- scripts.js
+++ scripts.js
@@ -1,9 +1,12 @@
 const cards = document.querySelectorAll('.memory-card');
 
 let hasFlippedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 function flipCard() {
+    if(lockBoard) return;
+    if(this === firstCard) return;
     this.classList.add('flip');
 
     if (!hasFlippedCard) {
@@ -15,22 +18,34 @@ function flipCard() {
         hasFlippedCard = false;
         secondCard = this;
         
-        if ( firstCard.dataset.framework === secondCard.dataset.framework) {
-            firstCard.removeEventListener('click', flipCard);
-            secondCard.removeEventListener('click', flipCard);
-        } else {
-            setTimeout(() => {
-                firstCard.classList.remove('flip');
-                secondCard.classList.remove('flip');
-            }, 1500)
-            
+        checkMatch();
 
-
-        };
-        console.log('Function works !!!');
     };
 };
 
+const checkMatch = () => {
+    if ( firstCard.dataset.framework === secondCard.dataset.framework) {
+        firstCard.removeEventListener('click', flipCard);
+        secondCard.removeEventListener('click', flipCard);
+    } else {
+        lockBoard = true;
+        setTimeout(() => {
+            firstCard.classList.remove('flip');
+            secondCard.classList.remove('flip');
+            lockBoard = false;
+        }, 1500);
+        
+    };
+};
 
+const shuffle = () => {
+
+    cards.forEach(card => {
+        let randomPosition =Math.floor(Math.random() * 12);
+        card.style.order =randomPosition;
+    });
+};
+
+shuffle();
 cards.forEach(card => card.addEventListener('click',flipCard));
 
