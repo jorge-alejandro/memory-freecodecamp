bee932a1c06f8fb94b52c31df268cfcb18c15d0e
Yorch <20454656+jorge-alejandro@users.noreply.github.com>
2023-04-26 07:46:45 +0200

    pop up de comienzo del juego y del final

--- js/cats.js
+++ js/cats.js
@@ -1,4 +1,4 @@
-// declare vairbles for cards, cards flipped, for locking the board , and for identifying both cards clicked
+// Declare variables for cards, cards flipped, for locking the board, and for identifying both cards clicked
 const cards = document.querySelectorAll('.memory-card');
 let hasFlippedCard = false;
 let lockBoard = false;
@@ -12,104 +12,97 @@ const triesSpan = document.getElementById('tries');
 let time = 0;
 let timer;
 
+// Add listener over the cards to be able to play with them
+cards.forEach(card => card.addEventListener('click', flipCard));
 
-// we add listener over the cards to be able to play with them
-
-cards.forEach(card => card.addEventListener('click',flipCard));
-
-// this functions inclludes the property flips to the cards so the effect is added using the CSS
+// This function includes the property flips to the cards so the effect is added using the CSS
 function flipCard() {
-    if(lockBoard) return;
-    //if(this === firstCard) return;
-    this.classList.add('flip');
+  if (lockBoard) return;
+  //if(this === firstCard) return;
+  this.classList.add('flip');
 
-    if (!hasFlippedCard) {
-        hasFlippedCard = true;
-        firstCard = this;
-    } else {
-        hasFlippedCard = false;
-        secondCard = this;
-        
-        checkMatch();
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+    hasFlippedCard = false;
+    secondCard = this;
 
-    };
-};
+    checkMatch();
+  }
+}
 
-//This function is meant to check if both card selected by the users has the same image. If both cards are the same the listener are erased so both card remain flipped. If not both card return to originalposition.
+// This function is meant to check if both cards selected by the users have the same image.
+// If both cards are the same, the listener is erased so both cards remain flipped. If not, both cards return to the original position.
 const checkMatch = () => {
-    if (firstCard.dataset.framework === secondCard.dataset.framework) {
-        firstCard.removeEventListener('click', flipCard);
-        secondCard.removeEventListener('click', flipCard);
-        matches += 1;
-        matchesSpan.textContent = matches;
-        tries += 1;
-        triesSpan.textContent = tries;
-        finishGame();
-    } else {
-        lockBoard = true;
-        setTimeout(() => {
-            firstCard.classList.remove('flip');
-            secondCard.classList.remove('flip');
-            lockBoard = false;
-            misses += 1;
-            missesSpan.textContent = misses;
-            tries += 1;
-            triesSpan.textContent = tries;
-        }, 1500);
-    }
+  if (firstCard.dataset.framework === secondCard.dataset.framework) {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    matches += 1;
+    matchesSpan.textContent = matches;
+    tries += 1;
+    triesSpan.textContent = tries;
+    finishGame();
+  } else {
+    lockBoard = true;
+    setTimeout(() => {
+      firstCard.classList.remove('flip');
+      secondCard.classList.remove('flip');
+      lockBoard = false;
+      misses += 1;
+      missesSpan.textContent = misses;
+      tries += 1;
+      triesSpan.textContent = tries;
+    }, 1500);
+  }
 };
 
-//to make the card appear randmly
-
+// To make the cards appear randomly
 const shuffle = () => {
-
-    cards.forEach(card => {
-        let randomPosition =Math.floor(Math.random() * 12);
-        card.style.order =randomPosition;
-    });
+  cards.forEach(card => {
+    let randomPosition = Math.floor(Math.random() * 12);
+    card.style.order = randomPosition;
+  });
 };
-
-// we call the function to make the 
 
 shuffle();
 
-
-// check if the game is complete
-
+// Check if the game is complete
 const finishGame = () => {
-    if (matches === 6) {
-      console.log(`you win motherfucker`);
-      cards.forEach(card => card.addEventListener('click',flipCard));
+  if (matches === 6) {
+    document.getElementById('startBtn').style.display = 'none';
+    document.getElementById('playAgainBtn').style.display = 'block';
+    overlay.style.display = 'flex';
+    popup.style.display = 'flex';
+  }
+};
 
-        // Show the popup
-    const popup = document.getElementById('popup');
-    popup.style.display = 'block';
-  
-      // reset game
-      matches = 0;
-      misses = 0;
-      tries = 0;
-      matchesSpan.textContent = matches;
-      missesSpan.textContent = misses;
-      triesSpan.textContent = tries;
-      cards.forEach(card => {
-        card.classList.remove('flip');
-        card.addEventListener('click',flipCard);
-      });
-      shuffle();
-    };
-  };
-  
-//timer 
+const overlay = document.querySelector(".overlay");
+const popup = document.querySelector(".popup");
+const startBtn = document.querySelector("#startBtn");
+const playAgainBtn = document.getElementById("playAgainBtn");
 
-function startTimer() {
-  timer = setInterval(() => {
-    time++;
-    console.log(time);
-  }, 1000);
-}
+startBtn.addEventListener("click", () => {
+  overlay.style.display = "none";
+  popup.style.display = "none";
+});
 
-function stopTimer() {
-  clearInterval(timer);
-}
+playAgainBtn.addEventListener("click", () => {
+  overlay.style.display = "none";
+  popup.style.display = "none";
+  // Reset game
+  matches = 0;
+  misses = 0;
+  tries = 0;
+  matchesSpan.textContent = matches;
+  missesSpan.textContent = misses;
+  triesSpan.textContent = tries;
+  cards.forEach(card => {
+    card.classList.remove('flip');
+    card.addEventListener('click', flipCard);
+  });
+  shuffle();
+});
 
+overlay.style.display = "flex";
+popup.style.display = "flex";
